f0d09d5c913d903b3e85a826b4bae0da507f52fc
Adeleke Anuoluwapo <102337992+cryptanu@users.noreply.github.com>
2022-05-25 08:36:52 +0100

    Updated CA and API link

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x6082917d81fc8a671f26ADBB9F1e58Ea7F223b24";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://vercel.com/cryptanu/nft-minting-dapp2/7hPEXkEURG1ppv7ZJ135AREq7STT/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
